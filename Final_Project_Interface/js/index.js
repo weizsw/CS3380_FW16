96721ba6e64022ce78cf50851560b2264f1f6426
weizsw <weizsw888@gmail.com>
2016-11-03 16:46:35 -0500

    New version

--- Final_Project_Interface/js/index.js
+++ Final_Project_Interface/js/index.js
@@ -32,6 +32,35 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
 
 });
 
+$('.form2').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+      if ($this.val() === '') {
+          label.removeClass('active2 highlight');
+        } else {
+          label.addClass('active2 highlight');
+        }
+    } else if (e.type === 'blur') {
+      if( $this.val() === '' ) {
+        label.removeClass('active2 highlight'); 
+      } else {
+        label.removeClass('highlight');   
+      }   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+        label.removeClass('highlight'); 
+      } 
+      else if( $this.val() !== '' ) {
+        label.addClass('highlight');
+      }
+    }
+
+});
+
 $('.tab a').on('click', function (e) {
   
   e.preventDefault();
@@ -42,6 +71,21 @@ $('.tab a').on('click', function (e) {
   target = $(this).attr('href');
 
   $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
+
+$('.tab2 a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active2');
+  $(this).parent().siblings().removeClass('active2');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content2 > div').not(target).hide();
   
   $(target).fadeIn(600);
   
